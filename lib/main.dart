670144cecccc99e99e4d56037540689c66158bc4
HaithamAhmed01 <54605034+HaithamAhmed01@users.noreply.github.com>
2021-06-25 19:59:06 +0200

    splash and add icon

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,67 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    checkLocationServicesInDevice();
+  }
+
+
+  Future<void> checkLocationServicesInDevice() async {
+    Location location = new Location();
+
+    var _serviceEnabled = await location.serviceEnabled();
+
+    if (_serviceEnabled) {
+      var  _permissionGranted = await location.hasPermission();
+
+      if (_permissionGranted == PermissionStatus.granted) {
+        // _location = await location.getLocation();
+
+        // print(_location.latitude.toString() + " " + _location.longitude.toString());
+
+
+      } else {
+        _permissionGranted = await location.requestPermission();
+
+        if (_permissionGranted == PermissionStatus.granted) {
+          print('user allowed');
+        } else {
+          SystemNavigator.pop();
+        }
+      }
+    } else {
+      var _serviceEnabled = await location.requestService();
+
+      if (_serviceEnabled) {
+        var _permissionGranted = await location.hasPermission();
+
+        if (_permissionGranted == PermissionStatus.granted) {
+          print('user allowed before');
+        } else {
+          _permissionGranted = await location.requestPermission();
+
+          if (_permissionGranted == PermissionStatus.granted) {
+            print('user allowed');
+          } else {
+            SystemNavigator.pop();
+          }
+        }
+      } else {
+        SystemNavigator.pop();
+      }
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -26,60 +26,6 @@ class _NavState extends State<HomePage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    checkLocationServicesInDevice();
-  }
-
-
-  Future<void> checkLocationServicesInDevice() async {
-    Location location = new Location();
-
-   var _serviceEnabled = await location.serviceEnabled();
-
-    if (_serviceEnabled) {
-     var  _permissionGranted = await location.hasPermission();
-
-      if (_permissionGranted == PermissionStatus.granted) {
-        // _location = await location.getLocation();
-
-        // print(_location.latitude.toString() + " " + _location.longitude.toString());
-
-
-      } else {
-        _permissionGranted = await location.requestPermission();
-
-        if (_permissionGranted == PermissionStatus.granted) {
-          print('user allowed');
-        } else {
-          SystemNavigator.pop();
-        }
-      }
-    } else {
-     var _serviceEnabled = await location.requestService();
-
-      if (_serviceEnabled) {
-       var _permissionGranted = await location.hasPermission();
-
-        if (_permissionGranted == PermissionStatus.granted) {
-          print('user allowed before');
-        } else {
-          _permissionGranted = await location.requestPermission();
-
-          if (_permissionGranted == PermissionStatus.granted) {
-            print('user allowed');
-          } else {
-            SystemNavigator.pop();
-          }
-        }
-      } else {
-        SystemNavigator.pop();
-      }
-
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
